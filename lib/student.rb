d74cea91931dac29c4de4f396da8837bd2b6e903
Holden Altaffer <haltaffer@gmail.com>
2018-05-15 22:14:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,13 +11,13 @@ class Student
   end
 
   def self.create_table
-    sql = "SQL
+    sql = "
     CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     album TEXT
     )
-    SQL"
+    "
   DB[:conn].execute(sql)
  end
 end
